40c780b1b3140a00d5659d7459878eaf1cb11550
annalorincz <anna.lorincz2@stud.uni-corvinus.hu>
2022-09-30 15:02:31 +0200

    Megtörtént a fájlba írás.

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UserMaintenance
             InitializeComponent();
             label1.Text = Resource.FullName;
             button1.Text = Resource.Add;
+            button2.Text = Resource.Filewrite;      //9.2.
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -34,6 +36,23 @@ namespace UserMaintenance
             };
             users.Add(u);
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                using(StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.Default))
+                {
+                    foreach (var u in users)
+                    {
+                        sw.Write(u.ID);
+                        sw.Write(u.FullName);
+                        sw.WriteLine();
+                    }
+                }
+            }
         }
     }
 }
