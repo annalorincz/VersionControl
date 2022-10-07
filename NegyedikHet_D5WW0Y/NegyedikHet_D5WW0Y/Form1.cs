55b9f52ce4889f0eecc298929fac90492ceae724
annalorincz <anna.lorincz2@stud.uni-corvinus.hu>
2022-10-07 14:08:01 +0200

    CreateExcel függvény megírás

--- NegyedikHet_D5WW0Y/NegyedikHet_D5WW0Y/Form1.cs
+++ NegyedikHet_D5WW0Y/NegyedikHet_D5WW0Y/Form1.cs
@@ -26,11 +26,45 @@ namespace NegyedikHet_D5WW0Y
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()                     //4.3
         {
             Flats = context.Flat.ToList();              //4.4
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                //CreateTable();
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
