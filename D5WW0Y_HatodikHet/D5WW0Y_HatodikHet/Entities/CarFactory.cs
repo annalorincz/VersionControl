c35971ba7c60401d2229249af6e4f260fe0ebd8c
annalorincz <anna.lorincz2@stud.uni-corvinus.hu>
2022-11-03 17:23:38 +0100

    Car Factory létrehozása

---
+++ D5WW0Y_HatodikHet/D5WW0Y_HatodikHet/Entities/CarFactory.cs
@@ -0,0 +1,17 @@
+﻿using D5WW0Y_HatodikHet.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D5WW0Y_HatodikHet.Entities
+{
+    public class CarFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Car();
+        }
+    }
+}
