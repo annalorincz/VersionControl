4bb91f4f1ef0e9439129155b7f55b09ff4b29e5d
annalorincz <anna.lorincz2@stud.uni-corvinus.hu>
2022-11-03 16:58:39 +0100

    Lista ésBallFactory property létrehozás

--- D5WW0Y_HatodikHet/D5WW0Y_HatodikHet/Form1.cs
+++ D5WW0Y_HatodikHet/D5WW0Y_HatodikHet/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using D5WW0Y_HatodikHet.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,16 @@ namespace D5WW0Y_HatodikHet
 {
     public partial class Form1 : Form
     {
+
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory      //egy példány
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
