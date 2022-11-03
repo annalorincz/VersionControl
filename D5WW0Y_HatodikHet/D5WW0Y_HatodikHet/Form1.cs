59f1eaf9c64ade285b5bf6f279df99b3a39b5d2a
annalorincz <anna.lorincz2@stud.uni-corvinus.hu>
2022-11-03 17:45:05 +0100

    Termékválasztó gombok, DisplayNext() fv

--- D5WW0Y_HatodikHet/D5WW0Y_HatodikHet/Form1.cs
+++ D5WW0Y_HatodikHet/D5WW0Y_HatodikHet/Form1.cs
@@ -17,11 +17,16 @@ namespace D5WW0Y_HatodikHet
 
         List<Toy> _toys = new List<Toy>();
 
+        private Toy _nextToy;
+
         private IToyFactory _factory;
         public IToyFactory Factory      //egy példány
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { 
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
@@ -58,12 +63,22 @@ namespace D5WW0Y_HatodikHet
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Factory = new CarFactory();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Factory = new BallFactory();
+        }
 
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
