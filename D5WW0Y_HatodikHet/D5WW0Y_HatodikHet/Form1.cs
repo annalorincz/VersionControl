38efabb51510c2db1299385cdea20ea7fa035e8f
annalorincz <anna.lorincz2@stud.uni-corvinus.hu>
2022-11-03 17:50:37 +0100

    Színválasztó gomb

--- D5WW0Y_HatodikHet/D5WW0Y_HatodikHet/Form1.cs
+++ D5WW0Y_HatodikHet/D5WW0Y_HatodikHet/Form1.cs
@@ -80,5 +80,16 @@ namespace D5WW0Y_HatodikHet
             _nextToy.Left = label1.Left;
             Controls.Add(_nextToy);
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button3.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
