ba53b123e6c932949d07606a9edc493bf009b41a
annalorincz <anna.lorincz2@stud.uni-corvinus.hu>
2022-12-09 14:11:20 +0100

    Verseny felépítése

--- D5WW0Y_TizedikHet/D5WW0Y_TizedikHet/Form1.cs
+++ D5WW0Y_TizedikHet/D5WW0Y_TizedikHet/Form1.cs
@@ -73,16 +73,24 @@ namespace D5WW0Y_TizedikHet
 
 
             var winners = from p in topPerformers
-                          where p.IsWinner
+                          where !p.IsWinner
                           select p;
             if (winners.Count() > 0)
             {
                 winnerBrain = winners.FirstOrDefault().Brain.Clone();
                 gc.GameOver -= Gc_GameOver;
+                button1.Visible = true;
                 return;
             }
         }
 
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
+        }
     }
 }
